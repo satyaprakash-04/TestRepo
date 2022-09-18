997c8001b74332247747a77a59a361e56bac9d58
Satyaprakash Sahoo <sahoosatyaprakash44@gmail.com>
2022-09-18 16:18:39 -0700

    Commented all

--- pandademo2.py
+++ pandademo2.py
@@ -54,55 +54,55 @@ print(df)
 #
 # print('print events and day\n', df[['events', 'day']])
 
-# To get the particular Row
-
-print('the index \'2\' row:\n', df.loc[2])
-
-print('to print the temperature column: ', df['temperature'])
-
-# To print the maximum temperature
-
-print('the maximum temperature is: ', df['temperature'].max())
-
-# to print the minimum temperature.
-
-print('the minimum temperature is: ', df['temperature'].min())
-
-# To print the average temperature
-
-print('The average Temperature is: ', df['temperature'].mean())
-
-# To get the description of the dataset
-
-print('The description of the dataset is: \n', df.describe())
-
-# To get temperature greater and  equals 34
-
-print('the temperature >= 32 is: \n', df[df.temperature >= 34])
-
-#  if the column contains spaces then use
-
-# print('The temperature is: ', df[df.temperature == df['temperature'] >= 33])
-
-#  By default pandas set the index from 0-x
-# to see the index use
-print('the index vvalue is: ', df.index)
-#  To Change the index to the specified field.
-
-print('the specified field index is: \n', df.set_index('day'))
-# The above operation returns the new dataframe but does not modify it.
-# To change the dataframe and get the modification result
-
-print('To modify the dataframe for the index is: ', df.set_index('day', inplace=True))
-#  NOw print dataframe and the location of the dataframe.
-print('The dataframe: \n', df)
-print('the specified row is by using the index is :\n', df.loc['12-09-2022'])
-
-# To reset the index to the original one use
-
-df.reset_index(inplace=True)
-#  Now print Dataframe
-print(df)
+# # To get the particular Row
+#
+# print('the index \'2\' row:\n', df.loc[2])
+#
+# print('to print the temperature column: ', df['temperature'])
+#
+# # To print the maximum temperature
+#
+# print('the maximum temperature is: ', df['temperature'].max())
+#
+# # to print the minimum temperature.
+#
+# print('the minimum temperature is: ', df['temperature'].min())
+#
+# # To print the average temperature
+#
+# print('The average Temperature is: ', df['temperature'].mean())
+#
+# # To get the description of the dataset
+#
+# print('The description of the dataset is: \n', df.describe())
+#
+# # To get temperature greater and  equals 34
+#
+# print('the temperature >= 32 is: \n', df[df.temperature >= 34])
+#
+# #  if the column contains spaces then use
+#
+# # print('The temperature is: ', df[df.temperature == df['temperature'] >= 33])
+#
+# #  By default pandas set the index from 0-x
+# # to see the index use
+# print('the index vvalue is: ', df.index)
+# #  To Change the index to the specified field.
+#
+# print('the specified field index is: \n', df.set_index('day'))
+# # The above operation returns the new dataframe but does not modify it.
+# # To change the dataframe and get the modification result
+#
+# print('To modify the dataframe for the index is: ', df.set_index('day', inplace=True))
+# #  NOw print dataframe and the location of the dataframe.
+# print('The dataframe: \n', df)
+# print('the specified row is by using the index is :\n', df.loc['12-09-2022'])
+#
+# # To reset the index to the original one use
+#
+# df.reset_index(inplace=True)
+# #  Now print Dataframe
+# print(df)
 
 # Whenever setting the index if column contains the duplicate value the it adds the default index
 # followed by the set index.
